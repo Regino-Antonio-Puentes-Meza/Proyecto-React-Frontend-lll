e080948254670c1baf548144c1af3ec8c646c4fc
Regino-Antonio-Puentes-Meza <32297255+Regino-Antonio-Puentes-Meza@users.noreply.github.com>
2022-09-02 12:59:53 -0500

    Update Listado.js

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -8,17 +8,11 @@ import Item from './Item';
 // MÉTODOS: Listado no requiere de métodos.
 // PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
 export default function Listado({ changeCantidad }) {
-	// Exportar stock a Item para que pueda ser usado por el componente Item.
-	// Exportar handleClick a Item para que pueda ser usado por el componente Item.
-	// useState es un hook que nos permite crear un estado para un componente.
-
 	return (
 		<div className='container'>
 			{/* renderizamos los Item aquí */}
-			{/* El parametro item contendrá toda la info del JSON */}
 			{Registros.map((item) => {
 				return (
-					// Acá se renderiza cada Item y le pasamos por props el método para aumentar el estado de App.
 					<Item
 						changeCantidad={changeCantidad}
 						item={item}
